5f3b350484617f445de9c9f80d07f14ada0d93da
Paul Dix <paul@pauldix.net>
2014-08-10 14:59:11 -0400

    blerg more stuff.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,7 @@
 //= require_tree .
+$(function() {
+  var s = skrollr.init();
+  var homeHeight = $("#home").height();
+  $(".wrapper").css("margin-top", homeHeight + "px");
+  console.log("DONE!")
+});
