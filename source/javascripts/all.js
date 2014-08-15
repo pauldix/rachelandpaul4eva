4ae939ec8afdb146c9615296ec42b59621e718ba
Paul Dix <paul@pauldix.net>
2014-08-14 21:30:57 -0400

    bleh

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,20 @@
 //= require_tree .
 $(function() {
+  // var windowHeight = $(window).height();
+  // $("#our_story").css("margin-top", windowHeight + "px");
+  var storyWidth = $(".story").width();
+  if (storyWidth < 1111) {
+    storyWidth = 1111;
+  }
+  storyLeft = storyWidth / 2 - 375 - 10;
+  storyRight = storyWidth / 2 - 250 + 30;
+  picLeft = storyWidth / 2 - 250;
+  picRight = storyWidth / 2 - 375 + 30
+  // $(".story-left").attr("data-center-top", "left:" + storyLeft + "px;");
+  // $(".story-right").attr("data-center-top", "left:" + storyRight + "px;");
+  // $(".pic-left").attr("data-center-top", "left:" + picRight + "px;");
+  // $(".pic-right").attr("data-center-top", "left:" + picLeft + "px;");
+
   var s = skrollr.init();
-  var homeHeight = $("#home").height();
-  $(".wrapper").css("margin-top", homeHeight + "px");
   console.log("DONE!")
 });
