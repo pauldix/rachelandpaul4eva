6f5a25a3241779485435432c19299bf02ec28e10
Paul Dix <paul@pauldix.net>
2014-08-14 22:26:11 -0400

    Fixed parallax. did stuff

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,20 +1,5 @@
 //= require_tree .
-$(function() {
-  // var windowHeight = $(window).height();
-  // $("#our_story").css("margin-top", windowHeight + "px");
-  var storyWidth = $(".story").width();
-  if (storyWidth < 1111) {
-    storyWidth = 1111;
-  }
-  storyLeft = storyWidth / 2 - 375 - 10;
-  storyRight = storyWidth / 2 - 250 + 30;
-  picLeft = storyWidth / 2 - 250;
-  picRight = storyWidth / 2 - 375 + 30
-  // $(".story-left").attr("data-center-top", "left:" + storyLeft + "px;");
-  // $(".story-right").attr("data-center-top", "left:" + storyRight + "px;");
-  // $(".pic-left").attr("data-center-top", "left:" + picRight + "px;");
-  // $(".pic-right").attr("data-center-top", "left:" + picLeft + "px;");
-
+$(window).load(function() {
   var s = skrollr.init();
   console.log("DONE!")
 });
