d81f9d3019dc452fdee76ff40adba12929e94aad
Paul Dix <paul@pauldix.net>
2014-08-14 23:01:20 -0400

    add controls and animations for wedding party images

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,13 @@
 //= require_tree .
 $(window).load(function() {
   var s = skrollr.init();
-  console.log("DONE!")
+
+  $(".images .circular").on("click", function(ev) {
+    $(ev.target).siblings().removeClass("selected");
+    $(ev.target).addClass("selected");
+    var offset = $(ev.target).attr("data-offset") * 500 * -1;
+    $(ev.target).parent().next().children().animate({"margin-left": offset});
+    console.log("CLICK");
+  });
+  console.log("DONE!");
 });
