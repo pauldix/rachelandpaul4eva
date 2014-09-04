707c66d2e0b05575521fcdac251b9ccfdd8e40de
Paul Dix <paul@pauldix.net>
2014-09-04 08:08:27 -0400

    Make nav links scroll and stuff

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -10,14 +10,35 @@ $(window).load(function() {
     console.log("CLICK");
   });
 
-  $("ul.tabrow li").click(function(e) {
-    e.preventDefault();
-    $("ul.tabrow li").removeClass("selected");
-    $(this).addClass("selected");
+  $("a").click(function(ev) {
+    var i = ev.target.href.indexOf("#");
+    if (i != -1) {
+      ev.preventDefault();
+      var elId = ev.target.href.slice(i, ev.target.href.length)
+      var top = $(elId).position().top - 40;
+      if(history.pushState) {
+          history.pushState(null, null, elId);
+      }
+      $('html, body').animate({
+        scrollTop: top
+      }, 2000);
+    }
   });
+
   $("#loading_title p").hide();
   clearInterval(window.loadingIndicator);
+  $("html, body").scrollTop(0);
   $("#loading_title").fadeOut(1600);
-  $("#loading_cover").fadeOut(800);
+  $("#loading_cover").fadeOut(800, function() {
+    var elId = window.location.hash;
+    if (elId) {
+            var top = $(elId).position().top - 40;
+      $('html, body').animate({
+        scrollTop: top
+      }, 2000);
+
+    }
+  });
+
   console.log("DONE!");
 });
