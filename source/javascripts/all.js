ea2305311fedaea44d34b6a0299b2d5b74fa67d8
Paul Dix <paul@pauldix.net>
2014-09-14 12:57:20 -0400

    Add submenu navigation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,6 +7,35 @@ $(window).load(function() {
     e.parent().width(e.width() + 124);
   });
 
+  $("h2 a").hover(function(ev) {
+    console.log("hover");
+    window.ev = ev;
+    var nav = $(ev.target).attr("data-nav");
+    if (nav != "") {
+      clearTimeout(window.navHide);
+      $(".nav-sub").hide();
+      var menu = $("ul." + nav);
+      menu.css({top: "55px", left: $(ev.target).position().left});
+      if (nav == "nola") {
+        $(menu).width("140px");
+      } else {
+        $(menu).width($(ev.target).width());        
+      }
+      menu.slideDown();
+    }
+  }, function(ev) {
+    window.navHide = setTimeout(function() {
+      $(".nav-sub").hide();
+    }, 300);
+    console.log("leave")
+  });
+
+  $(".nav-sub").hover(function(ev) {
+    clearTimeout(window.navHide);
+  }, function(ev) {
+    $(".nav-sub").hide();
+  });
+
   var s = skrollr.init();
 
   $(".images .circular").on("click", function(ev) {
@@ -16,12 +45,23 @@ $(window).load(function() {
     $(ev.target).parent().next().children().animate({"margin-left": offset});
   });
 
+  var getTopOffset = function(elId) {
+    var el = $(elId);
+    var topMargin = el.css("marginTop").replace('px', '');
+    var topPadding = el.css("paddingTop").replace('px', '');
+    console.log("* ", topMargin, topPadding);
+    var headingHeight = 80;
+    var positionOffset = headingHeight - topMargin - topPadding;
+    return el.position().top - positionOffset;
+  };
+
   $("a").click(function(ev) {
     var i = ev.target.href.indexOf("#");
     if (i != -1) {
       ev.preventDefault();
       var elId = ev.target.href.slice(i, ev.target.href.length)
-      var top = $(elId).position().top - 40;
+      var top = getTopOffset(elId);
+      $(".nav-sub").hide();
       if(history.pushState) {
           history.pushState(null, null, elId);
       }
@@ -38,7 +78,7 @@ $(window).load(function() {
   $("#foo, #progress").fadeOut(800, function() {
     var elId = window.location.hash;
     if (elId) {
-            var top = $(elId).position().top - 40;
+      var top = getTopOffset(elId);
       $('html, body').animate({
         scrollTop: top
       }, 2000);
